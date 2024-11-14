ae395f2a90e84c8d8be80c991cba48314c304ef8
정재웅 <jweidon0821@gmail.com>
2024-11-14 15:41:09 +0900

    chore: CORS allowed Origin 환경변수 적용하여 추가

--- src/main/java/com/sherpa/jodaeri/config/CorsConfig.java
+++ src/main/java/com/sherpa/jodaeri/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://127.0.0.1:3000", "http://localhost:3000", "https://jodaeri.vercel.app", "https://jodaeri.vercel.app/")
+                .allowedOriginPatterns("http://127.0.0.1:3000", "http://localhost:3000", "https://jodaeri.vercel.app", "https://jodaeri-bz9mktmmx-jjs-projects-d0f0b737.vercel.app/")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
