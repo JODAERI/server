c7a8de43f2e3701b0eac22c21b39a5561c531217
정재웅 <jweidon0821@gmail.com>
2024-11-29 00:23:20 +0900

    chore: 관련 웹사이트 CORS 허용

--- src/main/java/com/sherpa/jodaeri/config/CorsConfig.java
+++ src/main/java/com/sherpa/jodaeri/config/CorsConfig.java
@@ -15,7 +15,15 @@ public class CorsConfig implements WebMvcConfigurer {
                         "http://localhost:3000",
                         "https://jodaeri.vercel.app",
                         "https://jodaeri-bz9mktmmx-jjs-projects-d0f0b737.vercel.app/",
-                        "chrome-extension://cekenpinaifhijpikjjkngfiljpkgkjd")
+                        "chrome-extension://cekenpinaifhijpikjjkngfiljpkgkjd",
+                        // 허용 사이트
+                        "https://www.signgate.com",
+                        "https://www.signkorea.com ",
+                        "https://www.crosscert.com",
+                        "https://www.tradesign.net",
+                        "https://www.g2b.go.kr",
+                        "https://www.pps.go.kr"
+                )
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
